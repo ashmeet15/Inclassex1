b300bbb7198610c096334ac20a1d6cd5312e6cfd
Ashmeet Kaur <kau15556@sheridancollege.ca>
2024-02-04 23:51:22 -0500

    first commit

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -4,11 +4,15 @@
  */
 package card;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author srinivsi
+ * @author Mayhansh Kaplsh
+ * Student Id: 991717781
  */
 public class CardTrick {
     
@@ -19,14 +23,56 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
+            
+            Random random = new Random();
+            c.setValue(random.nextInt(13)+1);
+            c.setSuit(Card.SUITS[random.nextInt(4)]);
+            magicHand[i]=c;
+
+
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Insert value for card");
+        int userValue=sc.nextInt();
+        sc.nextLine();
+        System.out.print("Enter the suit");
+        String userSuit = sc.nextLine();
+        sc.close();
         
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-        // add one luckcard hard code 2,clubs
+        Card userCard = new Card();
+        userCard.setSuit(userSuit);
+        userCard.setValue(userValue);
+        System.out.println(userCard);
+
+        Card magicCard = new Card();
+        magicCard.setValue(2);
+        magicCard.setSuit("Diamonds");
+        
+        for (Card hand1 : magicHand) {
+            
+            System.out.println(hand1);
+            if (magicCard.getSuit()==hand1.getSuit()){
+                if (magicCard.getValue()==hand1.getValue()){
+                    
+                printInfo();
+                break;
+                }
+            }
+        }
+        
+        
     }
-    
-}
+        
+        private static void printInfo() {
+            System.out.println("You guessed it right");
+        }
+
+        
+ 
+
+
+        
+ 
+    }
